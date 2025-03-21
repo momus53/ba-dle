a395663816b6ddaaa31af35670edbbf4f955a83b
momus53 <joakomatto@gmail.com>
2025-03-21 12:35:39 -0300

    bootstrap agregado

--- src/main.js
+++ src/main.js
@@ -9,74 +9,6 @@ import { setupPlay3, stop3 } from './play3.js'
 import { setupPlay4, stop4} from './play4.js'
 
 
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div style="display: flex; align-items: center; justify-content: space-between; margin-top:-30px;">
-      <button id="leftButton" type="button" style="border-radius: 30px">x</button>
-      <h1>Bañdle</h1>
-      <button id="rightButton" type="button" style="border-radius: 30px">?</button>
-    </div>
-    
-    <div class="card" id="card6" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.4); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between; border: 2px solid rgba(0, 40, 218, 0.5);">
-      <p id="pista6" style="margin-left: 10px;">Año: 2017 | Visitas en YouTube: 16M</p>
-    </div>
-
-    <div class="card" id="card1" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.18); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between;">
-      <p id="pista1" style="margin-left: 10px;">Pista 1: Bajo</p>
-      <button id="play1" type="button" style="border-radius: 30px; background-color: white; margin-right: -25px;" disabled>
-        <img src="play-icon.svg" alt="Play Icon" style="width: 35px; height: 35px;">
-      </button>
-    </div>
-    <div class="card" id="card2" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.18); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between;">
-      <p id="pista2" style="margin-left: 10px;">Pista 2: Percusión</p>
-      <button id="play2" type="button" style="border-radius: 30px; background-color: white; margin-right: -25px;" disabled>
-        <img src="play-icon.svg" alt="Play Icon" style="width: 35px; height: 35px;">
-      </button>
-    </div>
-    <div class="card" id="card3" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.18); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between;">
-      <p id="pista3" style="margin-left: 10px;">Pista 3: Piano + Guitarra</p>
-      <button id="play3" type="button" style="border-radius: 30px; background-color: white; margin-right: -25px;" disabled>
-        <img src="play-icon.svg" alt="Play Icon" style="width: 35px; height: 35px;">
-      </button>
-    </div>
-    <div class="card" id="card4" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.18); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between;">
-      <p id="pista4" style="margin-left: 10px;">Pista 4: Voces</p>
-      <button id="play4" type="button" style="border-radius: 30px; background-color: white; margin-right: -25px;" disabled>
-        <img src="play-icon.svg" alt="Play Icon" style="width: 35px; height: 35px;">
-      </button>
-    </div>
-    <div class="card" id="card5" style="border-radius: 30px; background-color:rgba(240, 240, 240, 0.18); margin-bottom: 5px; height: 1px; display: flex; align-items: center; justify-content: space-between;">
-      <p id="pista5" style="margin-left: 10px;">Pista 5: Pista del nombre o algo</p>
-    </div>
-
-    <div id="texto" class="card" style="border-radius: 30px; margin-top: -10px;margin-left: -15px;">
-      <input type="text" id="textInput" placeholder="¿Que canción es?¿Que artista es?" style="width: 100%; padding: 10px; border-radius: 5px; border: 1px solid #ccc;">
-      <div id="suggestions" style="border: 1px solid #ccc; border-radius: 5px; display: none; position: absolute; background-color: white; z-index: 1000;">
-        <!-- Suggestions will be inserted here -->
-      </div>
-    </div>
-
-    <div class="card" style="border-radius:0px; margin-top: -80px;">
-      <h3 id="win1"></h3>
-      <h3 id="win2"></h3>
-    </div>
-
-    <div class="button-container" style="margin-top: -40px;">
-      <div class="card" style="border-radius: 10px;">
-        <button id="skip" type="button"></button>
-      </div>
-      <div class="card" style="border-radius: 10px;">
-        <button id="guess" type="button"></button>
-      </div>
-    </div>
-    <p class="read-the-docs">
-      mis sitios web etc
-    </p>
-  </div>
-`
-
 const suggestions = document.getElementById('suggestions');
 const textInput = document.getElementById('textInput');
 
@@ -292,3 +224,5 @@ window.onclick = function(event) {
     modal.style.display = 'none';
   }
 };
+
+getElementById('play1').disabled = true;
